2cbb52fdfae6b05f5698b8e50de9d0c5ea38ad20
Sebastian Goodrick <sebastian@goodrick.ch>
2015-11-27 11:11:40 +0100

    Release

--- www/components/version/version-directive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('BrewControl.version.version-directive', [])
-
-.directive('appVersion', ['version', function(version) {
-  return function(scope, elm, attrs) {
-    elm.text(version);
-  };
-}]);
--- www/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('BrewControl.version', [
-  'BrewControl.version.interpolate-filter',
-  'BrewControl.version.version-directive'
-])
-
-.value('version', '0.1');
